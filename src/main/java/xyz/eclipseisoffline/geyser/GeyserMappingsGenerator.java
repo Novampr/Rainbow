974f5524835cb204f4431fc5d47350102eb3a95b
Eclipse <eclipse@eclipseisoffline.xyz>
2025-03-16 10:32:38 +0000

    Initial commit

---
+++ src/main/java/xyz/eclipseisoffline/geyser/GeyserMappingsGenerator.java
@@ -0,0 +1,11 @@
+package xyz.eclipseisoffline.geyser;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GeyserMappingsGenerator implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
