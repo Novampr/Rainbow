31acd52acbd2c7fb2d61d4021eac1d3a142d0635
Eclipse <eclipse@eclipseisoffline.xyz>
2025-09-06 06:17:04 +0000

    Migrate build scripts to kotlin, update to Gradle 9.0.0

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    id("fabric-loom") version "1.11-SNAPSHOT"
+}
+
+val minecraftVersion = properties["minecraft_version"]!! as String
+val parchmentVersion = properties["parchment_version"]!! as String
+val loaderVersion = properties["loader_version"]!! as String
+
+val modVersion = properties["mod_version"]!! as String
+val supportedVersions = properties["supported_versions"]!! as String
+val archivesBaseName = properties["archives_base_name"]!! as String
+val fabricVersion = properties["fabric_version"]!! as String
+
+val targetJavaVersion = 21
+
+repositories {
+    maven {
+        name = "ParchmentMC"
+        url = uri("https://maven.parchmentmc.org")
+    }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${minecraftVersion}")
+    mappings(loom.layered {
+        officialMojangMappings()
+        parchment("org.parchmentmc.data:parchment-${parchmentVersion}@zip")
+    })
+
+    modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+}
+
+tasks {
+    processResources {
+        inputs.property("version", modVersion)
+        inputs.property("supported_versions", supportedVersions)
+        inputs.property("loader_version", loaderVersion)
+        filteringCharset = "UTF-8"
+
+        filesMatching("fabric.mod.json") {
+            expand(
+                mapOf(
+                    "version" to modVersion,
+                    "supported_versions" to supportedVersions,
+                    "loader_version" to loaderVersion
+                )
+            )
+        }
+    }
+
+    jar {
+        from("LICENSE") {
+            rename { "${it}_${archivesBaseName}" }
+        }
+    }
+
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release = targetJavaVersion
+    }
+}
+
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+    }
+    withSourcesJar()
+}
+
+loom {
+    runs {
+        named("server") {
+            runDir = "run-server"
+        }
+    }
+}
