58b2c760176e99371782465df77c65cdcd886fd5
Eclipse <eclipse@eclipseisoffline.xyz>
2025-10-16 09:43:04 +0000

    Fix actions (I think)

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,3 +1,5 @@
+import net.fabricmc.loom.task.RemapJarTask
+
 plugins {
     id("rainbow.base-conventions")
     id("rainbow.publish-conventions")
@@ -7,4 +9,23 @@ dependencies {
     // Implement namedElements so IDEs can use it correctly, but include the remapped build
     implementation(project(path = ":rainbow", configuration = "namedElements"))
     include(project(":rainbow"))
+}
+
+tasks {
+    val copyJarTask = register<Copy>("copyRainbowClientJar") {
+        group = "build"
+
+        val remapJarTask = getByName<RemapJarTask>("remapJar")
+        dependsOn(remapJarTask)
+
+        from(remapJarTask.archiveFile)
+        rename {
+            "Rainbow.jar"
+        }
+        into(project.layout.buildDirectory.file("libs"))
+    }
+
+    named("build") {
+        dependsOn(copyJarTask)
+    }
 }
